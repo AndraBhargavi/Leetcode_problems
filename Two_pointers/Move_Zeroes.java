4a07f19f9e02dd324319ac3f39b4d634afa0cdda
AndraBhargavi <141841355+AndraBhargavi@users.noreply.github.com>
2025-07-25 14:29:01 +0530

    Create Move_Zeroes.java

---
+++ Two_pointers/Move_Zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        int right=1;
+        while(right<nums.length){
+            if(nums[left]!=0){
+                left++;
+                right++;
+            }
+            else if(nums[right]==0){
+                right++;
+            }
+            else{
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+            }
+        }
+           
+    }
+}
