be9bc3e7ded9fa9bd92665b665544b0e6f083915
Bhargavi12 <141841355+AndraBhargavi@users.noreply.github.com>
2026-08-14 06:35:13 +0530

    Time: 0 ms (100%), Space: 19.5 MB (29.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,23 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[0]*len(nums1)
-        
-        for i in range(len(nums1)):
-            y=self.ls(nums2,nums1[i])
-            ans[i]=y
-    
+        stack=[]
+        hashmap={}
+        for i in range(len(nums2)-1,-1,-1):
+            while stack and stack[-1]<=nums2[i]:
+                stack.pop()
+            if not stack:
+                hashmap[nums2[i]]=-1
+            else:
+                hashmap[nums2[i]]=stack[-1]
+            stack.append(nums2[i])
+        print(hashmap)
+        ans=[]
+        for i in nums1:
+            ans.append(hashmap[i])
         return ans
-    def ls(self,nums2,target):
-        index=-1
-        for i in range(len(nums2)):
-            if nums2[i]==target:
-                index=i
-                break
-        for i in range(index+1,len(nums2)):
-            if nums2[i]>target:
-                return nums2[i]
-        return -1
-        
-
-                
-        
+            
